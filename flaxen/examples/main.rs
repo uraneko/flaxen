8f74ac8c3bb35d7d6da77922535dd7fde0b21399
nekodusha <BOOTHAlessandra@tutanota.com>
2024-08-03 20:52:27 +0100

    update example

--- flaxen/examples/main.rs
+++ flaxen/examples/main.rs
@@ -1,5 +1,10 @@
-use lib::flaxen;
+use lib::{init, run};
 
 fn main() {
-    flaxen();
+    let (mut sol, mut i, mut h, mut ui) = init();
+    'main: loop {
+        let tokens = run(&mut i, &mut h, &mut sol, &mut ui);
+
+        // eprintln!("{:?}", tokens);
+    }
 }
