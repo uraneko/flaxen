fd447481ff51f8be1bb32375b5f0f7ad91fd57a8
uraneko <BOOTHAlessandra@tutanota.com>
2024-08-14 17:26:55 +0100

    update version to 0.3.1

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@
 compile_error!("feature ragout/custom_events and ragout/lib can not e enabled concurrently");
 
 #[cfg(feature = "custom_events")]
-pub use ragout_input_macro::ragout_input;
+pub use ragout_custom_events_macro::ragout_custom_events;
 
 #[cfg(feature = "lib")]
 pub mod ragout;
