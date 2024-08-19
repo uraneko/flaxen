ab10423989ce2be138504357976d9b19de5d4a16
uraneko <BOOTHAlessandra@tutanota.com>
2024-08-19 16:56:39 +0100

    clean up entire repo

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,3 @@
-#[cfg(all(feature = "lib", feature = "custom_events"))]
-compile_error!("feature ragout/custom_events and ragout/lib can not e enabled concurrently");
+pub mod shin_sekai;
 
-#[cfg(feature = "custom_events")]
-pub use ragout_custom_events_macro::ragout_custom_events;
-
-#[cfg(feature = "lib")]
-pub mod ragout;
-#[cfg(feature = "lib")]
-pub use ragout::*;
-
-#[cfg(feature = "styled")]
-pub mod styled;
-#[cfg(feature = "styled")]
-pub use styled::*;
+struct Terminal;
